90fe048943855356027f0af9cbe0bcc65aba0047
Ryan Rose <ryanthrose@gmail.com>
2017-02-07 19:21:52 -0600

    Updated .gitignore and some assets.

---
+++ Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour {
+
+	public bool left;
+	public float initialSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
