534d27b40cfd473767cb00dcd89b755ee61bfad8
shuhaokou <shuhaokou@gmail.com>
2017-02-08 21:28:23 -0600

    Add bird picture and ScoreCounter

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+	public GUIText scoreGT;
+
 	void Start () {
+
+		GameObject scoreGo = GameObject.Find ("ScoreCounter");
+
+		scoreGT = scoreGo.GetComponent<GUIText> ();
+
+		//scoreGT.text = "0";
 		
 	}
 	
@@ -14,5 +22,9 @@ public class Bird : MonoBehaviour {
 
 	void OnMouseEnter() {
 		Destroy (this.gameObject);
+
+		int score = int.Parse (scoreGT.text);
+		score += 1;
+		scoreGT.text = score.ToString ();
 	}
 }
